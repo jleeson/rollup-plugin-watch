a05c0fa8bc81eb963fc098c6df00182af618e038
jleeson <jleeson2000@gmail.com>
2023-09-13 11:34:00 -0700

    updated dependencies + cleaned up package

---
+++ rollup.config.mjs
@@ -0,0 +1,17 @@
+import esbuild from "rollup-plugin-esbuild";
+import { builtinModules } from "module";
+import fs from "fs";
+
+const { dependencies } = JSON.parse(fs.readFileSync(new URL("./package.json", import.meta.url)));
+
+export default {
+    input: "src/index.js",
+    output: [
+        { file: "dist/plugin.mjs", format: "esm" },
+        { file: "dist/plugin.js", format: "cjs", exports: "default" }
+    ],
+    plugins: [
+        esbuild({ target: "node16" })
+    ],
+    external: builtinModules.concat(Object.keys(dependencies))
+};
